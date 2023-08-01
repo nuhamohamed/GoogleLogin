d200dfe34514e8c6fedc79a7f881bdd2b76d0538
Nuha <nuhasafia123@gmail.com>
2023-08-01 14:35:30 -0700

    Initial Commit

---
+++ GoogleLogin/GoogleLoginApp.swift
@@ -0,0 +1,17 @@
+//
+//  GoogleLoginApp.swift
+//  GoogleLogin
+//
+//  Created by Nuha Mohamed on 8/1/23.
+//
+
+import SwiftUI
+
+@main
+struct GoogleLoginApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
