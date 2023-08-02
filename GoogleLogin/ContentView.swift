bf72c8d817f06ef2f90470b5323f381ded32da62
Nuha <nuhasafia123@gmail.com>
2023-08-02 14:27:16 -0700

    Completed login page, not modified yet

--- GoogleLogin/ContentView.swift
+++ GoogleLogin/ContentView.swift
@@ -5,12 +5,41 @@
 //  Created by Nuha Mohamed on 8/1/23.
 //
 
+
+
 import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+       
+        NavigationView {
+        VStack{
+            NavigationLink(destination: HomePage()) {
+                Text("Home")
+            }
+            Text("Re-Do")
+                .font(.title)
+                .fontWeight(.bold)
+                .padding()
+                
+            Text("Login")
+                .padding(30)
+        Link(destination: URL(string: "https://accounts.google.com/")!, label: {
+            Text("Sign in with Google")
+                .fontWeight(.bold)
+                .multilineTextAlignment(.trailing)
+                
+            
+            
+        })
+        .frame(width: 280, height: 50)
+        .foregroundColor(.black)
+        .background(Color.white)
+        .border(Color.gray)
+        .cornerRadius(20)
+        
+        }
+}
     }
 }
 
@@ -18,4 +47,5 @@ struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
-}
+        }
+
