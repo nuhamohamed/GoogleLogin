6e9cf5133f62f7abc10f7479687a274e32772f5b
Garima Sunuwar <sunuwargarima@gmail.com>
2023-08-03 15:39:02 -0700

    google, design only

--- GoogleLogin/ContentView.swift
+++ GoogleLogin/ContentView.swift
@@ -14,30 +14,42 @@ struct ContentView: View {
        
         NavigationView {
         VStack{
-            NavigationLink(destination: HomePage()) {
-                Image(systemName: "house.fill")
-
+            if #available(iOS 15.0, *) {
+                NavigationLink(destination: HomePage()) {
+                    Image(systemName: "house.fill")
+                        .foregroundColor(Color(hue: 0.589, saturation: 0.737, brightness: 0.631))
+                        .font(.system(size:28))
+                    
+                }
+            } else {
+                // Fallback on earlier versions
             }
             Text("Re-Do")
-                .font(.title)
+                .font(.largeTitle)
                 .fontWeight(.bold)
+                .foregroundColor(Color(hue: 0.676, saturation: 0.81, brightness: 0.306))
                 .padding()
                 
             Text("Login")
+                .font(.title3)
                 .padding(30)
-        Link(destination: URL(string: "https://accounts.google.com/")!, label: {
+        Link(destination: URL(string: "https://accounts.google.com/")!, label:
+                {
+          Image("google.icon")
             Text("Sign in with Google")
-                .fontWeight(.bold)
-                .multilineTextAlignment(.trailing)
+                .fontWeight(.semibold)
+                .foregroundColor(Color(hue: 1.0, saturation: 0.0, brightness: 0.474))
+            
+//                .multilineTextAlignment(.trailing)
                 
             
             
         })
-        .frame(width: 280, height: 50)
+        .frame(width: 225, height: 50)
         .foregroundColor(.black)
         .background(Color.white)
-        .border(Color.gray)
-        .cornerRadius(20)
+        .border(Color(hue: 1.0, saturation: 0.038, brightness: 0.577))
+        .cornerRadius(5)
         
         }
 }
--- GoogleLogin/HomePage.swift
+++ GoogleLogin/HomePage.swift
@@ -7,51 +7,63 @@
 
 import SwiftUI
 
+@available(iOS 15.0, *)
 struct HomePage: View {
     @State var showDetails = false
+    @State private var randomQuoteIndex = 0
     var randomQuotes = ["You are awesome!", "You are amazing!", "You deserve the world!", "You got this!", "Push through!"]
+    var quote = ""
+    
     var body: some View {
         TabView{
-                VStack{
-                    Text("Welcome, [name]!")
-                        .font(.largeTitle)
-                        .fontWeight(.bold)
-                        .foregroundColor(Color(red: 0.1, green: 0.2, blue: 0.5, opacity: 1.0))
-                    
-                        .padding(50)
-                    
-                  
-                    Button( action: {
-                        self.showDetails.toggle()
-                    }) {
-                        Text("Quote OTD")
-                            .font(.headline)
-                            .fontWeight(.regular)
-                       //     .buttonStyle(.borderedProminent)
-                            .overlay(
-                                RoundedRectangle(cornerRadius: 10)
-                                .stroke(Color.blue, lineWidth: 1)
-                            )
-                            .padding(10)
+            VStack{
+                Text("Welcome, [name]!")
+                    .font(.largeTitle)
+                    .fontWeight(.bold)
+                    .foregroundColor(Color(hue: 0.624, saturation: 0.507, brightness: 0.619))
+                
+                    .padding(50)
+                
+                
+                Button( action: {
+                    self.showDetails.toggle()
+                    if self.showDetails {
+                        self.randomQuoteIndex = Int.random(in: 0..<randomQuotes.count)
                     }
-                   
-                    if showDetails {
-                        Text("Random Quotes")
-                    }
-
-                    
-    
+                }) {
+                    Text("Quotes")
+                        .foregroundColor(Color(hue: 0.625, saturation: 0.762, brightness: 0.848))
+                        .padding(10)
+                }
+                .overlay(
+                    RoundedRectangle(cornerRadius: 10)
+                        .stroke(Color.gray, lineWidth: 2)
+                )
+                if showDetails {
+                    Text(randomQuotes[randomQuoteIndex])
+                        .font(.body)
+                        .foregroundColor(.blue)
                     
                 }
+                
+                Spacer()
         
+        
+                        
+                        
+                
+                
+                
+            }
             
             
-
             
-                .tabItem(){
-                    Image(systemName: "house.fill")
-                    Text("Home")
-                }
+            
+            
+            .tabItem(){
+                Image(systemName: "house.fill")
+                Text("Home")
+            }
             
             Assignment()
                 .tabItem(){
@@ -68,9 +80,11 @@ struct HomePage: View {
     }
     
     
+    @available(iOS 15.0, *)
     struct HomePage_Previews: PreviewProvider {
         static var previews: some View {
             HomePage()
         }
     }
+    
 }
